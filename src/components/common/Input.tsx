91ccdf6e808d2dde42488f9ef6a82b41b1b8ee48
1-blue <1-blue98@naver.com>
2022-08-16 10:40:34 +0900

    feat: "Address Page"
    1. 실제 주소지 가져오는 기능 추가 ( "react-daum-postcode" )
    2. 주소지 읽기/쓰기/수정/삭제 기능 작성

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -22,7 +22,7 @@ const Input = ({
     <>
       <label
         htmlFor={name}
-        className="min-w-[300px] max-w-[600px] w-full font-bold cursor-pointer"
+        className="min-w-[200px] max-w-[600px] w-full font-bolder text-xs xs:text-sm md:text-base cursor-pointer"
       >
         {name}
       </label>
@@ -31,11 +31,11 @@ const Input = ({
         id={name}
         placeholder={placeholder}
         {...register}
-        className="min-w-[300px] max-w-[600px] w-full px-4 py-2 font-semibold text-base rounded-sm border-2 border-gray-200 focus:border-blue-400 focus:outline-none placeholder:text-sm"
+        className="min-w-[200px] max-w-[600px] w-full px-2 xs:px-4 py-2 font-semibold text-xs xs:text-base rounded-sm border-2 border-gray-200 focus:border-blue-400 focus:outline-none placeholder:text-[8px] xs:placeholder:text-xs"
         disabled={disabled}
       />
-      <span className="text-red-600 mb-4 mt-1 font-semibold text-sm">
-        {errorMessage}
+      <span className="self-start text-red-600 mb-4 mt-1 font-semibold text-[8px] xs:text-xs">
+        {errorMessage && "※" + " " + errorMessage}
       </span>
     </>
   );
